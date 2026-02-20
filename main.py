2cd883d57632d587882b70f2b494c72797e2dd45
Adeeb <adikikahani@gmail.com>
2026-02-20 08:59:36 +0530

    add stream endpoint

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from ytmusicapi import YTMusic
 from cachetools import TTLCache
-
+import requests
 app = FastAPI()
 
 app.add_middleware(
